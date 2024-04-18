0742e8a624c744423891e8db10911778f2eb207a
Yash57698 <yashsabale57698@gmail.com>
2024-04-18 13:52:03 +0530

    Added movement + screenscroll

--- main.py
+++ main.py
@@ -1,28 +1,99 @@
 import pygame
+import random
 from utilityfunctions import *
 
 SCREENSIZE = (1280,720)
-
+TOTALMAZESIZE = (1920,1920)
 pygame.init()
 screen = pygame.display.set_mode(SCREENSIZE)
+backgroundelements = pygame.Surface(TOTALMAZESIZE)
+foregroundelements = pygame.Surface(TOTALMAZESIZE,pygame.SRCALPHA)
 clock = pygame.time.Clock()
 running = True
      
 
 tiles = loadTileMap('./Assets/kenney_tinyDungeon/Tilemap/tilemap_packed.png')
+print(tiles[0].get_rect().size)
+map = [[0,0,0,0,0,1,0,0,0],
+       [1,1,1,0,1,1,1,1,0],
+       [0,0,0,0,1,0,1,0,0],
+       [0,0,0,0,0,0,0,0,0],
+       [0,0,0,0,0,1,1,0,0],
+       [0,0,0,0,0,1,0,0,0],
+       [0,0,0,0,0,0,0,0,0]]
+
+map = scalemapup(map)
+
+class Player:
+    bounds = (200,100)
+    pos = pygame.Vector2(0,0)
+    offset= pygame.Vector2(0,0)
+    minoffset = pygame.Vector2(0,0)
+    vel = 300
+    curvel = pygame.Vector2(0,0)
+    flipped = False
+
+    def __init__(self,image,pos):
+        self.pos = pos
+        self.image = image
+
+    def render(self,screen):
+        #TO-DO : Idle animation + hopanimation
+
+        screen.blit(self.image if self.flipped else pygame.transform.flip(self.image,True,False),(self.pos[0],self.pos[1]))
+
+    def move(self,dt):
+        keys = pygame.key.get_pressed()
+        
+        dx,dy = 0,0
+        if keys[pygame.K_w]:
+            dy = - self.vel* dt
+        if keys[pygame.K_s]:
+            dy = self.vel * dt
+        if keys[pygame.K_a]:
+            dx = -self.vel * dt
+        if keys[pygame.K_d]:
+            dx = self.vel * dt
+
+
+        if dx <0 :
+            self.flipped = False
+        if dx > 0:
+            self.flipped = True
+
+        self.pos += pygame.Vector2(dx,dy)
+        if self.pos[0] -((SCREENSIZE[0]/2) + p.offset.x) > self.bounds[0] and dx >0:
+            self.offset.x +=  dx
+        if self.pos[0] -((SCREENSIZE[0]/2) + p.offset.x) < -self.bounds[0] and dx <0:
+            self.offset.x +=  dx
+
+        if self.pos[1] -((SCREENSIZE[1]/2) + p.offset.y) > self.bounds[1] and dy > 0:
+            self.offset.y += dy
+        if self.pos[1] -((SCREENSIZE[1]/2) + p.offset.y) < -self.bounds[1] and dy < 0:
+            self.offset.y += dy
+
+        if self.offset.x < self.minoffset.x : self.offset.x = self.minoffset.x
+        if self.offset.y < self.minoffset.y : self.offset.y = self.minoffset.y
+        
+        self.curvel = (dx,dy)
+
+p = Player(tiles[96],(300,300))
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
     dt = clock.tick(60) / 1000
+    
+    
+    renderMap(map,backgroundelements,tiles)
+    foregroundelements.fill((0,0,0,0))
+    p.render(foregroundelements)
+    
 
-    # player postion update
-    for y in range(11):
-        for x in range(12):
-            screen.blit(tiles[y*12 + x],(64*x , 64*y))
-    # for i in range(132):
-    #     screen.blit(tiles[i],(0,0))
+    screen.blit(backgroundelements,-p.offset)
+    screen.blit(foregroundelements,-p.offset)
+    p.move(dt)
     pygame.display.flip()
         # time.sleep(2)
 
--- utilityfunctions.py
+++ utilityfunctions.py
@@ -1,4 +1,5 @@
 import pygame
+import random
 
 def loadTileMap(file):
     size = (64,64)
@@ -13,10 +14,78 @@ def loadTileMap(file):
     image = pygame.transform.scale_by(pygame.image.load(file),4.0)
     rect = image.get_rect()
     print(rect.size)
-    for y in range(y0,rect.height,dy+1):
-        for x in range(x0,rect.width,dx+1):
-            tile = pygame.Surface(size)
+    for y in range(y0,rect.height,dy):
+        for x in range(x0,rect.width,dx):
+            tile = pygame.Surface(size,pygame.SRCALPHA)
             tile.blit((image),(0,0),(x,y,*(size)))
             tiles.append(tile)
     print(f"loaded tileset with {len(tiles)} tiles from file : {file}")
     return tiles
+
+def scalemapup(map):
+    newmap = []
+    for i in range(len(map)*3):
+        row = []
+        for j in range(len(map[0])*3):
+            row.append(0)
+        newmap.append(row)
+
+    for y in range(len(map)):
+        for x in range(len(map[0])):
+            center = (1+(3*y),1+(3*x))
+            if(map[y][x] == 1):
+                for p in range(-1,2):
+                    for q in range(-1,2):
+                        newmap[center[0]+p][center[1]+q] = 1
+    return newmap
+
+def renderMap(map,screen,tiles):
+    for y in range(len(map)):
+        for x in range(len(map[0])):
+            if map[y][x] == 0:
+                random.seed(((len(map))*y)+x)
+                ty = random.randint(0,9)
+                if ty<2:
+                    screen.blit(tiles[49],(64*x,64*y))
+                else:
+                    screen.blit(tiles[48],(64*x,64*y))
+            if map[y][x] == 1:
+                if y!=0 and x!=0 and map[y-1][x] == 0 and map[y][x-1] ==0:
+                    screen.blit(tiles[4],(64*x,64*y)) #top left roof
+                elif y!=0 and x!=(len(map[0])-1) and map[y-1][x] == 0 and map[y][x+1] ==0:
+                    screen.blit(tiles[5],(64*x,64*y))#top right roof
+                elif y!=0 and map[y-1][x] == 0:
+                    screen.blit(tiles[26],(64*x,64*y))#top roof
+                elif y< (len(map)-2) and map[y+1][x] == 1 and map[y+2][x] == 0 and x!=0 and map[y][x-1] == 0:
+                    screen.blit(tiles[16],(64*x,64*y))#bottom left roof
+                elif y< (len(map)-2) and map[y+1][x] == 1 and map[y+2][x] == 0 and x!=(len(map[0])-1) and map[y][x+1] == 0:
+                    screen.blit(tiles[17],(64*x,64*y))#bottom right roof
+                elif y!=(len(map)-1) and map[y+1][x] == 0 and x!=0 and map[y][x-1] == 0:
+                    screen.blit(tiles[57],(64*x,64*y))#bottom left wall
+                elif y!=(len(map)-1) and map[y+1][x] == 0 and x!=(len(map[0])-1) and map[y][x+1] == 0:
+                    screen.blit(tiles[59],(64*x,64*y))#bottom right wall
+                elif y!=(len(map)-1) and map[y+1][x] == 0:
+                    screen.blit(tiles[40],(64*x,64*y))#bottom wall
+                elif y< (len(map)-2) and map[y+1][x] == 1 and map[y+2][x] == 0:
+                    screen.blit(tiles[2],(64*x,64*y))#bottom roof
+                elif x!=0 and (map[y][x-1] == 0 or (map[y][x-1] == 1 and y!=(len(map)-1) and map[y+1][x-1] == 0)):
+                    screen.blit(tiles[15],(64*x,64*y))#left roof
+                elif x!=(len(map[0])-1) and (map[y][x+1] == 0 or (map[y][x+1] == 1 and y!=(len(map)-1) and map[y+1][x+1] == 0)):
+                    screen.blit(tiles[13],(64*x,64*y))#right roof
+                elif y< (len(map)-2) and map[y+1][x] == 1 and map[y+2][x] == 1 and map[y+2][x+1] == 0:
+                    screen.blit(tiles[1],(64*x,64*y))#left turn
+                elif x!=0 and y!=0 and map[y-1][x-1] == 0:
+                    screen.blit(tiles[27],(64*x,64*y))# turn
+                elif x!=(len(map[0])-1) and y!=0 and map[y-1][x+1] == 0:
+                    screen.blit(tiles[25],(64*x,64*y))# turn
+                elif x!=0 and y<(len(map)-2) and map[y][x-1] == 1 and map[y+1][x-1] == 1 and map[y+2][x-1] == 0:
+                    screen.blit(tiles[3],(64*x,64*y))# turn
+                else :
+                    random.seed(((len(map))*y)+x)
+                    ty = random.randint(0,100)
+                    if ty<5:
+                        screen.blit(tiles[12],(64*x,64*y))
+                    elif ty<10:
+                        screen.blit(tiles[24],(64*x,64*y))
+                    else:
+                        screen.blit(tiles[0],(64*x,64*y))
